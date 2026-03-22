3839f085af8c24fa488272b03813c1127fd6219c
Akinori ADACHI <adachi.c.akinori@gmail.com>
2026-03-22 14:16:13 +0900

    Update SHA256 (sort + dnd + dual sound)

--- Casks/sessiondock.rb
+++ Casks/sessiondock.rb
@@ -1,6 +1,6 @@
 cask "sessiondock" do
   version "0.1.0"
-  sha256 "24979ddf8351b131118f3598654f6d1c2bd3e6fbefaa47691a6c8c39346f31d0"
+  sha256 "3eee66102d6450651348eec1df105558bc808a074f0ce6df6b38ff58bb1e82a6"
 
   url "https://github.com/adachic/sessiondock/releases/download/v#{version}/SessionDock_#{version}_aarch64.dmg"
   name "SessionDock"
